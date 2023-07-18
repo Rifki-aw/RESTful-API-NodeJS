0e1836dacbff4a8cb1066a61fea791287a06a29b
rifki <rifkibahrululum@gmail.com>
2023-07-18 21:08:20 +0700

    Setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-// 1. import modul express
+// 1. import framework express
 const express = require('express');
 
-// 2. membuat aplikasi express
+// 2. membuat aplikasi dari express
 const app = express();
 
-// 
+// 3. membuat PORT lokal
+const PORT = 5000;
+
+// 4. membuat listen yang dijalankan di lokal
+app.listen(PORT, () => {
+    console.log(`Server telah berhasil jalan di port ${PORT}!!`);
+});
